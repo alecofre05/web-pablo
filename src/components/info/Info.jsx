94f304eca5246bbe597951baeb76ca4212aa53c1
alejandro <cofrealejandro05@gmail.com>
2024-02-11 02:36:46 -0300

    cambios en los numeros

--- src/components/info/Info.jsx
+++ src/components/info/Info.jsx
@@ -2,13 +2,14 @@ import './info.css'
 
 export const Info = () => {
 
-    const phone = "1138207357"
+    const phone = "+5491138207357";
+    const encodedPhone = encodeURIComponent(phone);
     const email = "pablogcofre@gmail.com";
 
     return (
         <nav className="navbar" id="nb-color">
             <a className="nav-link anchor" id="mail-info" href={`https://mail.google.com/mail/?view=cm&fs=1&to=${email}`} target="_blank" rel="noopener noreferrer"><i class="fa fa-envelope-o"></i>{email}</a>
-            <a className="nav-link" id="phone-info" aria-current="page" href={`https://api.whatsapp.com/send?phone=+549${phone}`} target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-whatsapp"></i>11-3820-7357 Consultanos</a>
+            <a className="nav-link" id="phone-info" aria-current="page" href={`https://api.whatsapp.com/send?phone=${encodedPhone}`} target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-whatsapp"></i>11-3820-7357 Consultanos</a>
         </nav>
     )
 }
--- src/components/wsp/Wsp.jsx
+++ src/components/wsp/Wsp.jsx
@@ -1,13 +1,15 @@
+import React from 'react';
 import './wsp.css';
 
 export const Wsp = () => {
-    const phone = "1138207357";
+    const phone = "+5491138207357";
+    const encodedPhone = encodeURIComponent(phone);
 
     return (
         <>
-        <a href={`https://api.whatsapp.com/send?phone=+549${phone}`} className="float" target="_blank">
-            <i className="fa fa-whatsapp my-float"></i>
-        </a>
+            <a href={`https://api.whatsapp.com/send?phone=${encodedPhone}`} className="float" target="_blank">
+                <i className="fa fa-whatsapp my-float"></i>
+            </a>
         </>
-    )
-}
+    );
+};
