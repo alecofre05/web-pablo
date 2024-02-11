d457d9b2ff69cf6bc0b77298f59469e0ea2098a8
alejandro <cofrealejandro05@gmail.com>
2024-02-11 02:18:47 -0300

    cambio de prueba

--- src/components/info/Info.jsx
+++ src/components/info/Info.jsx
@@ -8,7 +8,7 @@ export const Info = () => {
     return (
         <nav className="navbar" id="nb-color">
             <a className="nav-link anchor" id="mail-info" href={`https://mail.google.com/mail/?view=cm&fs=1&to=${email}`} target="_blank" rel="noopener noreferrer"><i class="fa fa-envelope-o"></i>{email}</a>
-            <a className="nav-link" id="phone-info" aria-current="page" href={`https://api.whatsapp.com/send?phone=${phone}`} target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-whatsapp"></i>11-3820-7357 Consultanos</a>
+            <a className="nav-link" id="phone-info" aria-current="page" href={`https://api.whatsapp.com/send?phone=${phone}`} target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-whatsapp"></i>11-3820-7357 Consultanos.</a>
         </nav>
     )
 }
